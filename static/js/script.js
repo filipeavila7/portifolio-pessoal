91d04fc247bfee264c097ba5cd83fc566ed0b28b
filipeavila7 <filipeavila076@gmail.com>
2025-10-08 15:39:28 -0300

    continuando a responsividade

--- static/js/script.js
+++ static/js/script.js
@@ -82,3 +82,15 @@ indicators.forEach((dot, idx) => {
 
 // Inicializa
 updateCarousel();
+
+
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('.nav-links').classList.toggle('show');
+});
+
+
+document.querySelectorAll('.nav-links a').forEach(link => {
+    link.addEventListener('click', () => {
+        document.querySelector('.nav-links').classList.remove('show');
+    });
+});
